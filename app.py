9cef342ea8837aea0fa485736e978e9e8d641ae7
anubhi3107 <anubhi.vijayvergia@nutanix.com>
2018-08-22 15:58:34 +0530

    Fixed UI issues and added upload and update ruleset function

--- app.py
+++ app.py
@@ -1,77 +1,68 @@
-from datetime import datetime
+import os
 from logging import DEBUG
 from jsonschema import validate
-from flask import Flask, render_template, request, jsonify, Response
+from jsonschema.exceptions import ValidationError
+import json
+from flask import Flask, render_template, request,redirect, url_for, make_response, Response, flash
 
+UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__))
+ALLOWED_EXTENSIONS = set(['json'])
 app = Flask(__name__)
+app.secret_key = "super secret key"
 app.logger.setLevel(DEBUG)
-
-test_data = []
-
-
-def store_data(data):
-    testdata = {}
-    testdata.update({"test_url" : data["Test_url"]})
-    if data["Rule_set"]!="":
-        testdata.update({"rule_set": data["Rule_Set"]})
-    test_data.append(testdata)
-    app.logger.debug(test_data)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+test_data =[]
 
 
-def is_valid_json(json):
-    schema = """
-    {
-      "$schema": "http://json-schema.org/schema#",
-      "required": [
-        "Automation",
-        "Product"
-      ],
-      "type": "object",
-      "properties": {
-        "Product": {
-          "required": [
-            "Api",
-            "Ui"
-          ],
-          "type": "object",
-          "properties": {
-            "Api": {
-              "type": ["object", "array"]
-            },
-            "Ui": {
-              "type": ["object", "array"]
-            }
-          }
-        },
-        "Automation": {
-          "required": [
-            "Api",
-            "Ui"
-          ],
-          "type": "object",
-          "properties": {
-            "Api": {
-              "type": ["object", "array"]
-            },
-            "Ui": {
-              "type": ["object", "array"]
-            }
-          }
-        }
-      }
-    }
-    """
-    validate(json, schema)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def uploaded_file(filename):
+    file = json.loads(open(filename).read())
+    if is_valid_json(file):
+       return redirect(url_for('index'))
+    else:
+        if os.path.exists("Ruleset.json"):
+            os.remove("Ruleset.json")
+        return redirect(url_for('upload'))
+
+
+def is_valid_json(rulejson):
+    schema = json.loads(open("schema.json").read())
+    try:
+        validate(rulejson, schema)
+        return True
+    except ValidationError:
+        return False
+
+
+def update_ruleset(data):
+    ruleset = json.loads(open('Ruleset.json').read())
+    if data["errorCodeType"] == "list":
+        ruleset[data["errorLabel"]][data["errorType"]].append(data["errorThread"])
+        app.logger.debug(ruleset)
+    else:
+        ruleset[data["errorLabel"]][data["errorType"]].update({data["errorCode"] : data["errorThread"]})
+    app.logger.debug(ruleset)
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-        data =request.json
-        # TODO: Add rule set validation here
-        store_data(data)
-        app.logger.debug(data)
+        testdata = {}
+        testdata.update({"test_url":request.form["test_url"]})
+        is_auth = request.form.getlist("reqAuth")
+        if len(is_auth) != 0:
+            testdata.update({"username": request.form["username"], "password": request.form["password"]})
+        app.logger.debug(testdata)
+
+        if not os.path.exists("Ruleset.json"):
+            app.logger.debug("Redirecting to upload ruleset")
+            flash("Please upload ruleset first", "warning")
+            return redirect(url_for('upload'))
         return "Submitted"
     return render_template('index.html')
 
@@ -82,8 +73,6 @@ def update():
     return "Submitted"
 
 
-
-
 @app.route('/ruleset')
 def ruleset():
     return render_template('ruleset.html')
@@ -92,8 +81,29 @@ def ruleset():
 @app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
     if request.method == "POST":
+        update_ruleset(request.form)
+
         return "Submitted"
     return render_template('feedback.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    app.logger.debug("the request method for upload is " + request.method)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        app.logger.debug(file)
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            file.filename = "Ruleset.json"
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            uploaded_file("Ruleset.json")
+            return redirect(url_for('index'))
+    app.logger.debug("inside upload")
+    return render_template('upload.html')
 
 
 @app.errorhandler(404)
@@ -104,7 +114,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def server_error():
     return render_template('500.html'), 500
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/feedback.js
+++ static/js/feedback.js
@@ -1,45 +1,14 @@
-window.addEventListener("load", function () {
-  function sendData() {
-    var XHR = new XMLHttpRequest();
+function toggleVisibility(cb)
+ {
+  var error_code = document.getElementById("ecode");
 
-    // Bind the FormData object and the form element
-    var myForm = document.getElementById('updateForm');
-    console.log("here");
-    var formData = new FormData(myForm),
-    result = {};
-    console.log(formData.entries);
-    for (var entry of formData.entries())
-    {
-        result[entry[0]] = entry[1];
-    }
-    result = JSON.stringify(result)
-    console.log(result);
-
-    // Define what happens on successful data submission
-    XHR.addEventListener("load", function(event) {
-      alert(event.target.responseText);
-    });
-
-    // Define what happens in case of error
-    XHR.addEventListener("error", function(event) {
-      alert('Oops! Something went wrong.');
-    });
-
-    // Set up our request
-    XHR.open("POST", "http://127.0.0.1:5000/update");
-    XHR.setRequestHeader("Content-Type", "application/json")
-    // The data sent is what the user provided in the form
-    console.log("sending");
-    XHR.send(result);
+  if(cb.value=="list")
+  {
+    error_code.style.display = "none";
+    document.getElementById("errorCode").required = false;
   }
-
-  // Access the form element...
-  var form = document.getElementById("updateForm");
-
-  // ...and take over its submit event.
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    sendData();
-  });
-});
+  else{
+    error_code.style.display = "block";
+    document.getElementById("errorCode").required = true;
+  }
+ }
